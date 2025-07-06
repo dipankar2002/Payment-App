d20f8fe4de81010745970d996e3335d67f0d6fb7
dipankar2002 <dipankarganguly15@gmail.com>
2025-07-06 21:10:31 +0530

    add user check and all users routes

--- Backend/src/router/user.router.js
+++ Backend/src/router/user.router.js
@@ -1,14 +1,19 @@
 import express from "express";
 import {
+  checkUser,
   login,
   logout,
   signUp,
+  users,
 } from "../controllers/user.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 
+
+router.get("/check", authMiddleware, checkUser);
+router.get("/allUsers", authMiddleware, users);
 
 router.post("/signup", signUp);
 router.post("/login", login);
