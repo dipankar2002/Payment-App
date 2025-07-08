058a9f05d82213d80e06de84a47b5a10c08cb212
dipankar2002 <dipankarganguly15@gmail.com>
2025-07-08 23:36:37 +0530

    add transfer route

--- Backend/src/router/account.router.js
+++ Backend/src/router/account.router.js
@@ -1,7 +1,13 @@
 import express from "express";
 import { authMiddleware } from "../middleware/auth.middleware.js";
+import {
+  getAccountBalance,
+  transfer,
+} from "../controllers/account.controller.js";
 
 const router = express.Router();
+
+router.post("/transfer", authMiddleware, transfer);
 
 router.get("/balance", authMiddleware, getAccountBalance);
 
