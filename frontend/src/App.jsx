c571bb3c41c4c4ad2da01ed2c7901f64640cbe5a
dipankar2002 <dipankarganguly15@gmail.com>
2025-07-12 20:37:45 +0530

    refactor routing structure in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
-
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
 
   return (
     <div>
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage />:<Navigate to="login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/"/>} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/"/>} />
+      </Routes>
     </div>
   )
 }
