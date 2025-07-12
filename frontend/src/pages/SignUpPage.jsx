ff6759236d830873f5a509014aaa23604ced448e
dipankar2002 <dipankarganguly15@gmail.com>
2025-07-12 20:45:34 +0530

    add SignUpPage component with form handling and validation

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,9 +1,153 @@
-
+import React, { useState } from 'react'
+import { useAuthStore } from '../store/useAuthStore';
+import { Eye, EyeOff, Loader2, Lock, Mail, User } from 'lucide-react';
+import { Link } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
 const SignUpPage = () => {
+  const [ showPassword, setShowPassword ] = useState(false);
+  const [ formData, setFormData ] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const { signUp, isSigningUp } = useAuthStore();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    
+    signUp(formData);
+  }
 
   return (
-    <main></main>
+    <main className='h-screen'>
+      <Navbar />
+      <div className=' pt-20'>
+        <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+          <div className="w-full max-w-md space-y-8 px-6 py-10 rounded-2xl shadow-[0_0_15px_5px_#5754E8]">
+            {/* LOGO */}
+            <div className="text-center mb-8 flex flex-col items-center gap-2 group">
+              <h1 className="text-2xl font-bold mt-2">Create Account</h1>
+              <p className="text-base-content/60">Get started with your free account</p>
+            </div>
+            {/* FORM */}
+            <form onSubmit={handleSubmit} className="space-y-2">
+            <div className="flex gap-4">
+                {/* First Name */}
+                <div className="form-control w-1/2">
+                  <label className="label">
+                    <span className="label-text font-medium">First Name</span>
+                  </label>
+                  <div className="relative">
+                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                      <User className="size-5 text-base-content/40 z-10" />
+                    </div>
+                    <input
+                      required
+                      type="text"
+                      className="input input-bordered w-full pl-10"
+                      placeholder="John"
+                      value={formData.firstName}
+                      onChange={(e) => setFormData({ ...formData, firstName: e.target.value })}
+                    />
+                  </div>
+                </div>
+
+                {/* Last Name */}
+                <div className="form-control w-1/2">
+                  <label className="label">
+                    <span className="label-text font-medium">Last Name</span>
+                  </label>
+                  <div className="relative">
+                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                      <User className="size-5 text-base-content/40 z-10" />
+                    </div>
+                    <input
+                      required
+                      type="text"
+                      className="input input-bordered w-full pl-10"
+                      placeholder="Doe"
+                      value={formData.lastName}
+                      onChange={(e) => setFormData({ ...formData, lastName: e.target.value })}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text font-medium">Email</span>
+                </label>
+                <div className="relative">
+                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <Mail className="size-5 text-base-content/40 z-10" />
+                  </div>
+                  <input
+                    required
+                    type="email"
+                    className={`input input-bordered w-full pl-10`}
+                    placeholder="you@example.com"
+                    value={formData.email}
+                    onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                  />
+                </div>
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text font-medium">Password</span>
+                </label>
+                <div className="relative">
+                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <Lock className="size-5 text-base-content/40 z-10" />
+                  </div>
+                  <input
+                    required
+                    type={showPassword ? "text" : "password"}
+                    className={`input input-bordered w-full pl-10`}
+                    placeholder="••••••••"
+                    value={formData.password}
+                    onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                  />
+                  <button
+                    type="button"
+                    className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? (
+                      <EyeOff className="size-5 text-base-content/40 z-10" />
+                    ) : (
+                      <Eye className="size-5 text-base-content/40 z-10" />
+                    )}
+                  </button>
+                </div>
+              </div>
+
+              <button type="submit" className="btn btn-primary mt-4 w-full" disabled={isSigningUp}>
+                {isSigningUp ? (
+                  <>
+                    <Loader2 className="size-5 animate-spin" />
+                    Loading...
+                  </>
+                ) : "Create Account"}
+              </button>
+            </form>
+            {/* FOOTER */}
+            <div className="text-center">
+              <p className="text-base-content/60">
+                Already have an account?{" "}
+                <Link to="/login" className="link link-primary">
+                  Sign in
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
